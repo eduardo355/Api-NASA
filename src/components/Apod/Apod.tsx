eec2e7940063a2f3aa80dd5feff54b987b02878c
couch355 <ingeniero.eduardo355@gmail.com>
2024-03-24 01:21:15 -0600

    Mejoras en resposive

--- src/components/Apod/Apod.tsx
+++ src/components/Apod/Apod.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react"
 import { Apods } from "./TypeApod"
-
+import Loader from "../Loader/Loader"
 
 
 const Apod = () => {
     const [apod, setApod] = useState<Apods | null >(null)
+    const [loader, setLoader] = useState<boolean>(false)
 
     useEffect(() => {
+        setLoader(true)
         const getApod = () => {
             const URL = 'https://api.nasa.gov/planetary/apod?api_key=xO15lhanHARD6LlOCuvcLDbWgKt0cmXLr3nTaoKR'
             fetch(URL)
@@ -14,6 +16,7 @@ const Apod = () => {
             .then((data) => {
                 console.log(data);
                 setApod(data)
+                setLoader(false)
             })
         }
         getApod()
@@ -23,21 +26,25 @@ const Apod = () => {
         return
     }
     return (
-        <div className=" flex flex-col p-4">
+        <div className="flex flex-col p-4">
         <div className="flex">
             <small className="text-gray-500">Inicio / Imagen del dia</small>
         </div>
-            <div className=" flex items-center justify-center">
-                <div className=" flex flex-col items-center justify-center w-2/4 max-md:max-lg:w-[70%] max-sm:w-full ">
-                    <h1 className="text-3xl font-bold max-sm:text-2xl max-md:max-lg:text-2xl">{apod.title}</h1>
-                    <img src={apod.hdurl} alt={apod.title} className="object-cover" />
-                    <div className="flex p-2 gap-4 ">
+        {loader && <Loader />}
+        <div className=" flex items-center justify-center">
+            {!loader ?
+                <div className=" flex flex-col justify-center w-2/4 max-md:max-lg:w-[70%] max-sm:w-full ">
+                    <h1 className="text-3xl font-bold max-sm:text-2xl max-md:text-2xl max-lg:text-2xl">{apod.title}</h1>
+                    <img src={apod.hdurl} alt={apod.title} className="object-cover aspect-video" />
+                    <div className="flex my-2 gap-4 ">
                         <small>{apod.date}</small>
                         <small>copyright {apod.copyright}</small>
                     </div>
                     <p>{apod.explanation}</p>
                 </div>
+                : <span className="text-2xl">Buscando Imagen del Dia...</span>}
             </div>
+            
         </div>
     )
 }
--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -4,9 +4,9 @@ import Search from "./Search"
 const Nav = () => {
     const navegacion = useNavigate()
     return (
-        <div className="flex items-center justify-between p-5 max-sm:flex-col">
+        <div className="flex items-center justify-between p-5 max-sm:flex-col max-md:flex-col">
             <h1 className="text-4xl font-bold">NASA - API</h1>
-            <div className=" flex items-center justify-center gap-4">
+            <div className=" flex items-center justify-center gap-4 max-sm:flex-col">
                 <button onClick={() => navegacion('Apod')} className="relative overflow-hidden transition">
                     <span className="absolute inset-0 before:bg-black before:w-0 before:h-[1px] before:absolute before:left-0 before:bottom-0 before:transition-all hover:before:w-full"></span>
                     Imagen del día
